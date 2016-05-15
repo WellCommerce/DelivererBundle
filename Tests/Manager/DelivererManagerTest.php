34256a4433c8c2e942412a6d892b3c2c23da2916
Adam Piotrowski <adam@wellcommerce.org>
2016-05-15 18:52:05 +0200

    Fixed manager tests

---
+++ Tests/Manager/DelivererManagerTest.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ * 
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ * 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\DelivererBundle\Tests\Manager;
+
+use WellCommerce\Bundle\CoreBundle\Test\Manager\AbstractManagerTestCase;
+
+/**
+ * Class DelivererManagerTest
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class DelivererManagerTest extends AbstractManagerTestCase
+{
+    protected function get()
+    {
+        return $this->container->get('deliverer.manager');
+    }
+}
